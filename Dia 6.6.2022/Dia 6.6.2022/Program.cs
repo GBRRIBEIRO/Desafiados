860c647de6df624751d743f4313848bd3feb3624
Gabriel Nunes Ribeiro <102530919+GBRRIBEIRO@users.noreply.github.com>
2022-06-06 10:35:02 -0300

    Add files via upload

---
+++ Dia 6.6.2022/Dia 6.6.2022/Program.cs
@@ -0,0 +1,28 @@
+﻿double nota1; double nota2; double media;
+
+Console.WriteLine("Insira a primeira nota: ");
+nota1=Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Insira a segunda nota: ");
+nota2 = Convert.ToDouble(Console.ReadLine());
+
+media = (nota1 + nota2) / 2;
+
+/*
+if (media > 7)
+{
+    Console.WriteLine("Aprovado");
+}
+if (media <= 7 && media >5)
+{
+    Console.WriteLine("Recuperação");
+}
+if (media <= 5)
+{
+    Console.WriteLine("Reprovado");
+}*/
+
+//Ternario:
+
+string result = media <= 5 ? "Reprovado" : "Aprovado";
+Console.WriteLine(result);
